54338cea9d5c07cf616ae9e3eba863227344255f
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 13:16:01 +0530

    implementing addition

--- calculator.java
+++ calculator.java
@@ -1,6 +1,19 @@
+
+import java.util.Scanner;
+
 class calculator{
+    public static void sum(int x,int y){
+        System.out.println(x+y);
+    }
+
     public static void main(String args[]){
-    System.out.println("Calculator");
+        Scanner sc=new Scanner(System.in);
+    System.out.print("enter first no:");
+    int a=sc.nextInt();
+    System.out.println("enter 2nd no: ");
+    int b=sc.nextInt();
+    sum(a,b);
+
         
     }
 }
