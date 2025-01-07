28a08c880a4226a4b2bad8275403d4207acc9ae5
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 10:17:14 +0530

    2.100

--- first.java
+++ first.java
@@ -2,5 +2,6 @@ class first {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello prachciti");
+        System.out.println("prachiti");
     }
 }
