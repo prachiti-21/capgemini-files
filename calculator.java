180322153102d7e77657c807e05903c2fcb7b33d
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 13:24:55 +0530

    :calculator

--- calculator.java
+++ calculator.java
@@ -5,6 +5,16 @@ class calculator{
     public static void sum(int x,int y){
         System.out.println(x+y);
     }
+    public static void subtract(int x,int y) {
+      System.out.println(x-y);
+    }
+    public static void multiply(int x,int y) {
+    System.out.println(x*y);
+        
+    }
+    public static void division(int x,int y) {
+    System.out.println(x/y);  
+    }
 
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
@@ -13,6 +23,9 @@ class calculator{
     System.out.println("enter 2nd no: ");
     int b=sc.nextInt();
     sum(a,b);
+    subtract(a,b);
+    multiply(a,b);
+    division(a,b);
 
         
     }
