71867e2745aaa0b5afe1baa64f1156bb1a5020df
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 12:44:52 +0530

    calculator

---
+++ sum2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sum2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first no: ");
+        int a=sc.nextInt();
+        
+
+    }
+}
